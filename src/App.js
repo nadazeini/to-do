5a0ef7e787a78c3d2c57945fff1005c205ad753d
Nada Zeini <nada.elzeini@sjsu.edu>
2019-11-23 16:43:59 -0800

    adding to do item

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import React ,{Component} from 'react';
-import Header from './components/layout/Header';
-import './App.css';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-
-class App extends Component{
+import React, {Component} from "react";
+import Header from "./components/layout/Header";
+import "./App.css";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import uuid from 'uuid';
+class App extends Component {
   //states
   state = {
-    todos : [ //array of objects
-      {
-        //have id since most of the time you need a unique id for ressources
-        id: 1,
-        title:'Take out the trash',
-        completed: false
-
-      },
-      {
-        
-        id: 2,
-        title:'Dinner',
-        completed: false
-
-      },
-      {
-        
-        id: 3,
-        title:'Study',
-        completed: false
-
-      },
-//to access state this.state.whatever
+    todos: [
     ]
-  }
-  markComplete = (id) =>{
-//to test console.log
-this.setState({todos: this.state.todos.map(todo =>{
-  if(todo.id === id){
-    todo.completed = !todo.completed //whatever the opposite is
-  }
-  return todo;
-})})
-  }
-  deleteTodo = (id) =>{
-    //to test 
+  };
+  markComplete = id => {
+    //to test console.log
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed; //whatever the opposite is
+        }
+        return todo;
+      })
+    });
+  };
+  deleteTodo = id => {
+    //to test
     //console.log(id)
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id )]})
-  }
-  render(){
-   // console.log(this.state.todos);
-   //pass array as property 
-   return (
-      <div className="App">
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+  //Add to do
+  addTodo = (title) => {
+    console.log(title);
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    };
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        newTodo
+      ]
+    });
+  };
+  render() {
+    // console.log(this.state.todos);
+    //pass array as property
+    return (<div className="App">
+      {" "}
+      <div className="container">
+        {" "}
         <Header/>
-        <AddTodo/>
-        <Todos todos = {this.state.todos} markComplete={this.markComplete}
-        deleteTodo = {this.deleteTodo}/>
-      </div>
-    )
+        <AddTodo addTodo={this.addTodo}/>{" "}
+        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo}/>{" "}
+      </div>{" "}
+    </div>);
   }
 }
 
